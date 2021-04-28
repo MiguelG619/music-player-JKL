084a31d775a8ca83c361e5a1aeb95ff6708c17f2
MiguelG619 <69783877+MiguelG619@users.noreply.github.com>
2021-04-28 17:29:19 +0800

    updated models db.js

--- MCO2/models/db.js
+++ MCO2/models/db.js
@@ -1,10 +1,10 @@
-
-// import module `mongoose`
+// import module mongoose
 const mongoose = require('mongoose');
 
-// import module `User` from `../models/UserModel.js`
+// TODO insert other models
 const User = require('./UserModel.js');
 
+// TODO Check if this works
 // ccapdev-mongoose is the name of the database
 const url = 'mongodb://localhost:27017/jklDB';
 
@@ -14,10 +14,78 @@ const options = {
     useNewUrlParser: true
 };
 
+const database = {
+    
+    // connect
+    connect: function () {
+        mongoose.connect(url, options, function(error) {
+            if(error) throw error;
+            console.log('Connected to: ' + url);
+        });
+    },
 
+    // insertTrack
+    // insertPlaylist
+    // insertUser
+    insertOne: function(model, doc, callback) {
+        model.create(doc, function(error, result) {
+            if(error) return callback(false);
+            console.log('Added ' + result);
+            return callback(true);
+        });
+    },
 
+    // findTrack - to see if there are duplicate url's
+    // findUser - to see if there are duplicate users, unique username
+    findOne: function(model, query, projection, callback) {
+        model.findOne(query, projection, function(error, result) {
+            if(error) return callback(false);
+            return callback(result);
+        });
+    },
+
+    // findTracks - Home Search, callback return array of track objects
+    // findUsers - Home Search, callback return array of user objects
+    findMany: function(model, query, projection, callback) {
+        model.find(query, projection, function(error, result) {
+            if(error) return callback(false);
+            return callback(result);
+        });
+    },
+
+    // updateTrack
+    // updatePlaylist
+    // updateUser
+    updateOne: function(model, filter, update) {
+        model.updateOne(filter, update, function(error, result) {
+            if(error) return callback(false);
+            console.log('Document modified: ' + result.nModified);
+            return callback(true);
+        });
+    },
+
+    // deleteTrack
+    // deletePlaylist
+    // deleteUser
+    deleteOne: function(model, conditions) {
+        model.deleteOne(conditions, function (error, result) {
+            if(error) return callback(false);
+            console.log('Document deleted: ' + result.deletedCount);
+            return callback(true);
+        });
+    },
+
+    // deleteUserTracksPlaylist - delete all tracks and playlists with deleted username
+    deleteMany: function(model, conditions) {
+        model.deleteMany(conditions, function (error, result) {
+            if(error) return callback(false);
+            console.log('Document deleted: ' + result.deletedCount);
+            return callback(true);
+        });
+    }
+}
 /*
-    exports the object `database` (defined above)
+    exports the object database (defined above)
     when another script exports from this file
 */
 module.exports = database;
