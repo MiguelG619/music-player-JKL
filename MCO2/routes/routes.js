d85203f57bf03f54ae00d94a9b9e321215b37b2c
MiguelG619 <69783877+MiguelG619@users.noreply.github.com>
2021-05-22 23:04:55 +0800

    Revert "Revert "fixed profInfo.hbs""
    This reverts commit f1fee88d0354248c7d1705229838cfa1bbf3a408.

--- MCO2/routes/routes.js
+++ MCO2/routes/routes.js
@@ -21,17 +21,25 @@ const router = express.Router();
 
 // LogIn SignUp LogOut
 router.get("/getCheckUsername", signUpController.getCheckUsername);
+
 router.get("/signUp", signUpController.getSignUp);
 // router.post("/signUp", validation.signUpValidation(), signUpController.postSignUp);
+
 router.post("/signUp", signUpController.postSignUp);
+
 router.get("/", logInController.getLogIn);
+
 router.post("/logIn", logInController.postLogIn);
+
 router.get("/logOut", logInController.getLogOut);
 
 // searchTracks and searchArtists
 router.get("/searchTracks", searchTracksController.getAllTracks);
+
 router.get("/searchOneTrack", searchTracksController.getOneTrack);
+
 router.get("/searchArtists", searchArtistsController.getAllArtists);
+
 router.get("/searchOneArtists", searchArtistsController.getOneArtist);
 
 // profile
@@ -44,27 +52,39 @@ router.get("/profInfo", profileInfoController.getPersonalInfo);
 	
 */
 router.get("/profInfo/:username", profileInfoController.getOtherProfile);
+
 router.get("/profInfo/edit", profileEditController.getProfileEdit);
+
 router.patch("/profInfo/edit", profileEditController.updateProfile);
+
 router.delete("/editTrack/delete", profileEditController.deleteUser);
 
 //muscisPlayer/Tracks
 // router.get("/musicPlayer", musicPlayerController.getTrack);
 router.get("/musicPlayer/:id", musicPlayerController.getTrack);
+
 router.get("/upload", uploadTrackController.getUpload);
+
 router.post("/upload", uploadTrackController.postTrack);
 // <a href="editTrack/{{this._id}}</a> gawing anchr tag yung title ng track
 router.get("/editTrack/:id", editTrackController.getEditTrack);
+
 router.patch("/editTrack/:id", editTrackController.updateTrack);
+
 // <a href="/editTrack/delete/{{this._id}}" onclick="return confirm("Are you sure to delete this track?");><i logo></i></a>
 // gawing anchr tag yung delete button
+
 router.get("/editTrack/delete/:id", editTrackController.deleteTrack);
 
 // playlist
 router.get("/playlistView", playlistViewController.getAllPlaylists);
+
 router.get("/profInfo/playlist/tracks/:id", playlistViewController.getOnePlaylist);
+
 router.post("/playlistView/create", playlistViewController.postPlaylist);
+
 router.get("/playlistView/edit", playlistEditController.getPlaylistEdit);
+
 router.patch("/playlistView/edit", playlistEditController.updatePlaylist);
 // <a href="/playlistView/delete/{{this._id}}" onclick="return confirm("Are you sure to delete this track?");><i logo></i></a>
 // gawing anchr tag yung delete button
