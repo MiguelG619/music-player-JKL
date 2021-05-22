7fd4053f7eb866ee6781510350e7bf5a70b4140c
Jordansbg <jordan_m_sibug@dlsu.edu.ph>
2021-05-22 17:59:11 +0800

    fixed profInfo.hbs

--- MCO2/index.js
+++ MCO2/index.js
@@ -5,6 +5,8 @@ const session = require("express-session");
 const routes = require("./routes/routes");
 const mongoose = require("mongoose");
 const MongoStore = require("connect-mongo")(session);
+const path = require(`path`);
+
 
 const db = require("./models/db.js");
 
@@ -23,7 +25,7 @@ hbs.registerPartials(__dirname + "/views/partials");
 // parses incoming requests with urlencoded payloads
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // use `express-session`` middleware and set its options
 // use `MongoStore` as server-side session storage
